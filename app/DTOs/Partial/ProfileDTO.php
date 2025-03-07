6460df5997dd4713b3ae958c6a0d306a5371a638
gsdev <gumennikov24@yandex.ru>
2025-03-07 20:36:32 +0400

    Hotfix

--- app/DTOs/Partial/ProfileDTO.php
+++ app/DTOs/Partial/ProfileDTO.php
@@ -9,8 +9,8 @@ use Illuminate\Contracts\Support\Arrayable;
 readonly class ProfileDTO implements Arrayable
 {
     public function __construct(
-        public string $text,
-        public string $avatarUrl,
+        public ?string $text,
+        public ?string $avatarUrl,
         public array $educations,
         public array $workPlaces,
         public array $tools

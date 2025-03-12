ddfa474e2ee5960309733a1aaa4547d87111e06c
gsdev <gumennikov24@yandex.ru>
2025-03-12 18:40:16 +0400

    Remove unused observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use App\Models\WorkCase;
-use App\Observers\WorkCaseObserver;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
@@ -19,8 +17,6 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        WorkCase::observe(WorkCaseObserver::class);
-
         Builder::macro('getMappedWithMethod', fn (string $method, mixed ...$args): Collection => map_model_collection($this->get(), $method, $args));
     }
 }
